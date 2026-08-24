99adadbfb4d16725b9dfe64f27a4834ccaaf7985
Akanksha Singh <aakankshaasingh18@gmail.com>
2026-08-24 23:30:30 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (56.94%) - LeetSync

--- 1944-truncate-sentence/truncate-sentence.py
+++ 1944-truncate-sentence/truncate-sentence.py
@@ -1,4 +1,6 @@
 class Solution(object):
     def truncateSentence(self, s, k):
+        #truncate sentence 
         word= s.split()
-        return" " .join(word[:k])
+        return" " .join(word[:k]) 
+        
