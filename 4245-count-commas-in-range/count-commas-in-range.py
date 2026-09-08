db8ad60c742b9e4423b99b21fa5f3cc873449601
Akanksha Singh <aakankshaasingh18@gmail.com>
2026-09-08 22:23:18 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (100.00%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.py
+++ 4245-count-commas-in-range/count-commas-in-range.py
@@ -1,11 +1,13 @@
 class Solution(object):
     def countCommas(self, n):
         count=0
-        for i in range(1, n+1):
+        '''for i in range(1, n+1):
             if i>= 1000:
                 count +=1
-        return count
-
-
+        return count'''
+        if n < 1000:
+                return 0
+        else:
+            return n - 1000 + 1
        
         
